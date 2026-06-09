29eacf1f0c7480359e9c18a1363cd2a2a9fe23cf
azh <azhrockryeo02@gmail.com>
2026-06-09 13:20:43 +0800

    fix: use router.tsx in App, add Navbar layout, protect landing page

--- mock-interview-coach/src/app/App.tsx
+++ mock-interview-coach/src/app/App.tsx
@@ -1,30 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { InterviewProvider } from './providers/InterviewProvider'
 import { AuthProvider } from '../context/AuthContext'
-import LandingPage from '../pages/LandingPage'
-import Interview from '../pages/InterviewPage'
-import NotFoundPage from '../pages/NotFoundPage'
-import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
-import ProtectedRoute from '../components/shared/ProtectedRoute'
+import { router } from './router'
 import '../index.css'
 
 const App = () => (
   <AuthProvider>
     <InterviewProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/interview" element={
-            <ProtectedRoute>
-              <Interview />
-            </ProtectedRoute>
-          } />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </InterviewProvider>
   </AuthProvider>
 )
--- mock-interview-coach/src/app/router.tsx
+++ mock-interview-coach/src/app/router.tsx
@@ -1,4 +1,6 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Outlet } from 'react-router-dom'
+import Navbar from '../components/layout/Navbar'
+import ProtectedRoute from '../components/shared/ProtectedRoute'
 import LandingPage from '../pages/LandingPage'
 import InterviewPage from '../pages/InterviewPage'
 import ResultsPage from '../pages/ResultsPage'
@@ -8,65 +10,81 @@ import HistoryPage from '../pages/HistoryPage'
 import SessionDetailPage from '../pages/SessionDetailPage'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
-import ProtectedRoute from '../components/shared/ProtectedRoute'
 
-const router = createBrowserRouter([
+// Layout that includes the Navbar
+function RootLayout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
+}
+
+export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LandingPage />,
-  },
-  {
-    path: '/login',
-    element: <LoginPage />,
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />,
-  },
-  {
-    path: '/interview',
-    element: (
-      <ProtectedRoute>
-        <InterviewPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '/results',
-    element: (
-      <ProtectedRoute>
-        <ResultsPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '/progress',
-    element: (
-      <ProtectedRoute>
-        <ProgressPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '/history',
-    element: (
-      <ProtectedRoute>
-        <HistoryPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '/history/:id',
-    element: (
-      <ProtectedRoute>
-        <SessionDetailPage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: '*',
-    element: <NotFoundPage />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <ProtectedRoute>
+            <LandingPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/interview',
+        element: (
+          <ProtectedRoute>
+            <InterviewPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/results',
+        element: (
+          <ProtectedRoute>
+            <ResultsPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/progress',
+        element: (
+          <ProtectedRoute>
+            <ProgressPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/history',
+        element: (
+          <ProtectedRoute>
+            <HistoryPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/history/:id',
+        element: (
+          <ProtectedRoute>
+            <SessionDetailPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: '/login',
+        element: <LoginPage />,
+      },
+      {
+        path: '/register',
+        element: <RegisterPage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
+      },
+    ],
   },
 ])
-
-export default router
